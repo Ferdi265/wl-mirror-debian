860f15f4fa97c9f94c1efc26fffe9f4d790937a8
Ferdinand Bachmann <theferdi265@gmail.com>
2023-02-24 17:50:21 +0100

    debian: update to 0.13.0

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir -p build/ && cd build/
 
 # get source
-VERSION="${VERSION:-0.12.2}"
+VERSION="${VERSION:-0.13.0}"
 if [[ ! -f "wl-mirror_$VERSION.orig.tar.gz" ]]; then
     wget "https://github.com/Ferdi265/wl-mirror/releases/download/v$VERSION/wl-mirror-$VERSION.tar.gz" -O "wl-mirror_${VERSION}.orig.tar.gz"
     wget "https://github.com/Ferdi265/wl-mirror/releases/download/v$VERSION/wl-mirror-$VERSION.tar.gz.sig" -O "wl-mirror_${VERSION}.orig.tar.gz.sig"
